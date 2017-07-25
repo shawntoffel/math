59116023091ff602c20ec89a274f3688f34bf270
Shawn Toffel <shawn.toffel+github@gmail.com>
2017-07-25 16:50:16 -0600

    Create math.go

---
+++ math.go
@@ -0,0 +1,12 @@
+// binary powering algorithm for computing 'a' to the power of 'b' for integer values
+func Pow(a, b int) int {
+	p := 1
+	for b > 0 {
+		if b&1 != 0 {
+			p *= a
+		}
+		b >>= 1
+		a *= a
+	}
+	return p
+}
